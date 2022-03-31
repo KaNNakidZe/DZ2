9bf3ebe5b24cf4f358f77c2b7eefad818eae9881
KaNNaT <KaNNaT13@yandex.ru>
2022-03-31 22:42:26 +0300

    первый коммит

--- src/KaNNaT/first/Main.java
+++ src/KaNNaT/first/Main.java
@@ -3,6 +3,21 @@ package KaNNaT.first;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Задание № 1
+        byte apple = 3;
+        short pear = 207;
+        int cherry = 600;
+        long orange = 900L;
+        float watermelon = 1.5f;
+        double melon = 4.7;
+        boolean pearMore = pear < 300;
+        System.out.println(pear);
+        char dollar = 36;
+        System.out.println(dollar);
+
+    // Задание № 2
+
+
+
     }
 }
