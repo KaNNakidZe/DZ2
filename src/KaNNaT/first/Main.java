c4ad078b3a9b9c8e0de479a3371e9a4afa99ce88
KaNNaT <KaNNaT13@yandex.ru>
2022-03-31 23:59:56 +0300

    первый коммит

--- src/KaNNaT/first/Main.java
+++ src/KaNNaT/first/Main.java
@@ -3,25 +3,63 @@ package KaNNaT.first;
 public class Main {
 
     public static void main(String[] args) {
-	// Задание № 1
+	// Task # 1
         byte apple = 3;
         short pear = 207;
         int cherry = 600;
         long orange = 900L;
         float watermelon = 1.5f;
         double melon = 4.7;
-        boolean pearMore = pear < 300;
+        boolean pearMore = true;
         System.out.println(pear);
         char dollar = 36;
         System.out.println(dollar);
 
-    // Задание № 2
+    // Task # 2
         double boxerFirst = 78.2;
         double boxerSecond = 82.7;
         double ollBoxersWeight = boxerFirst + boxerSecond;
         System.out.println("Общий вес боксеров " + ollBoxersWeight + " кг!");
         double differenceWeight = boxerSecond - boxerFirst;
         System.out.println("Разница в весе боксеров " + differenceWeight + " кг!");
+
+    // Task # 3
+        int banana = 5;
+        int bananaWeight = 80;
+        int ollBananasWeight = banana * bananaWeight;
+        System.out.println("Общий вес бананов " + ollBananasWeight + " грамм!");
+
+        int milk = 2;
+        int oneMilkWeight = 105;
+        int ollMilkWeight = milk * oneMilkWeight;
+        System.out.println("Общий вес молока " + ollMilkWeight + " грамм!");
+
+        int icecream = 2;
+        int oneIcecream = 100;
+        int ollIcecreamWeight = icecream * oneIcecream;
+        System.out.println("Общий вес мороженного " + ollIcecreamWeight + " грамм!");
+
+        int rawegg = 4;
+        int oneRawegg = 70;
+        int ollRaweggWeight = rawegg * oneRawegg;
+        System.out.println("Общий вес сырых яйц " + ollRaweggWeight + " грамм!");
+
+        int breakfastWeight = ollBananasWeight + ollMilkWeight + ollIcecreamWeight + ollRaweggWeight;
+        System.out.println("Общий вес завтрака " + breakfastWeight + " грамм!");
+
+        int KgBreakfastwKgWeight = 1000;
+        float breakfastwKgWeight = breakfastWeight/(float)KgBreakfastwKgWeight;
+        System.out.println("Общий вес завтрака " + breakfastwKgWeight + " кг!");
+
+
+    // Task # 4
+
+
+
+
+
+
+
 
 
 
